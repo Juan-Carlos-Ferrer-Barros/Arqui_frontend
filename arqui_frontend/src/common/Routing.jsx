32d25457beefff2a384fcd1743ae772c000fd25a
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-13 22:36:46 -0400

    private page

--- arqui_frontend/src/common/Routing.jsx
+++ arqui_frontend/src/common/Routing.jsx
@@ -3,10 +3,18 @@ import Landing from '../landing/Landing'
 // import App from './App'
 import Navbar from './Navbar'
 // import { useState, useEffect } from "react"
+// import PrivatePage from '../auth/PrivatePage'
+
+/*
+Para hacer páginas privadas que sólo se puede llegar logueado,
+se hace así, donde Componente es la página que se quiere proteger:
+<PrivatePage component={Componente} />
+
+En (a) sería:
+<Route path={"{path}"} element={<PrivatePage component={Componente} />} />
+*/
 
 export default function Routing(){
-    
-    
     return (
         <>
         <Navbar />
@@ -14,7 +22,7 @@ export default function Routing(){
         <BrowserRouter>
             <Routes>
                 <Route path={'/'} element={<Landing/>}/>
-
+                {/* (a) */}
             </Routes>
         </BrowserRouter>
         </>
