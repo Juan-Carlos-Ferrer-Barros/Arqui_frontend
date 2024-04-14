42efc0a275a5ee789186af7f4f15f720b9644325
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-13 22:36:38 -0400

    login and register page

---
+++ arqui_frontend/src/pages/Login.jsx
@@ -0,0 +1,58 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
+import axios from 'axios';
+
+function Login() {
+    const { login } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, {
+            email,
+            password,
+        }).then((response) => {
+            if (response.status === 200) {
+                return login(response.data.userId, response.data.access_token);
+            }
+            console.error(response);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='email' className='form-email'>
+                    Email:
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='username@example.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+                <label htmlFor='password' className='form-password'>
+                    Password:
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                <button type='submit'>Login</button>
+                {/* <input type="submit" value="Submit" /> */}
+            </form>
+        </>
+    )
+}
+
+export default Login;
---
+++ arqui_frontend/src/pages/Register.jsx
@@ -0,0 +1,89 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
+import axios from 'axios';
+
+function Register() {
+    const { login } = useContext(AuthContext);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`, {
+            username,
+            email,
+            password,
+        }).then((response) => {
+            if (response.status === 200) {
+                return login(response.data.userId, response.data.access_token);
+            }
+            else if (response.status === 400) {
+                alert('User already exists');
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    return (
+        <>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='username' className='form-username'>
+                    Email:
+                    <input
+                        name='username'
+                        type='text'
+                        placeholder='username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </label>
+                <label htmlFor='email' className='form-email'>
+                    Email:
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='username@example.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+                <label htmlFor='password' className='form-password'>
+                    Password:
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                <label htmlFor='password' className='form-password'>
+                    Confirm Password:
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                <button type='submit'>Register</button>
+                {/* <input type="submit" value="Submit" /> */}
+            </form>
+        </>
+    );
+}
+
+export default Register;
