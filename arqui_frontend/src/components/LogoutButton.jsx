d89748773a8634c51b2c843cda48c1204ccd1e4e
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-13 20:35:19 -0400

    button for logout

---
+++ arqui_frontend/src/components/LogoutButton.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
+
+const LogoutButton = () => {
+    const { logout } = useContext(AuthContext);
+
+    return (
+        <button className='logout-button' onClick={logout}>
+            Logout
+        </button>
+    );
+};
+
+export default LogoutButton;
