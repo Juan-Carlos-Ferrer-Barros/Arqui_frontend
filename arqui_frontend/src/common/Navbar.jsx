d7ac86b9db2bda0bfc363fd4fa60db4a5cab428e
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-14 18:09:43 -0400

    login options

--- arqui_frontend/src/common/Navbar.jsx
+++ arqui_frontend/src/common/Navbar.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { useContext, useEffect } from 'react';
 import './Navbar.css';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
+import LogoutButton from '../components/LogoutButton';
+
+function LoggedOptions() {
+  return (
+      <>
+          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/users">Users</NavLink>
+          <LogoutButton />
+      </>
+  )
+}
+
+function UnloggedOptions() {
+  return (
+      <>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Register</NavLink>
+      </>
+  )
+}
 
 function Navbar() {
-
+  const { isLogged } = useContext(AuthContext);
 
   return (
+    <>
     <nav className="navbar">
+    <NavLink to="/">Home</NavLink>
+    {isLogged ? <LoggedOptions /> : <UnloggedOptions />}
       <ul className="navbar-list">
         <li className='navbar-item'>
           Hola
@@ -14,6 +39,7 @@ function Navbar() {
       
       </ul>
     </nav>
+    </>
   );
 }
 
