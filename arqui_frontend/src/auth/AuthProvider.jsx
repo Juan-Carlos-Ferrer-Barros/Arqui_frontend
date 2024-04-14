d87b210ba649165ac3c10600651a8c51b1adaaa0
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-13 20:35:00 -0400

    auth managers

---
+++ arqui_frontend/src/auth/AuthProvider.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+function AuthProvider({ children }) {
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
+    const [userId, setUserId] = useState(localStorage.getItem("userId") || null);
+    const [isLogged, setIsLogged] = useState(false);
+
+    useEffect(() => {
+        localStorage.setItem("token", token);
+    }, [token]);
+
+    useEffect(() => {
+        localStorage.setItem("userId", userId);
+    }, [userId]);
+
+    function logout() {
+        setToken(null);
+        setUserId(false);
+        isLogged(false);
+    }
+
+    function login(token, userId) {
+        setToken(token);
+        setUserId(userId);
+        setIsLogged(true);
+    }
+
+    return (
+        <AuthContext.Provider value={{ token, userId, isLogged, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
