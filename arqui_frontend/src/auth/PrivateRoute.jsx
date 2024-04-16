a2d2e2a5962fbff58bdf664061f84a9e42af16c3
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-04-16 01:49:48 -0400

    private route

---
+++ arqui_frontend/src/auth/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "./AuthContext";
+import PropTypes from "prop-types";
+
+const PageForbidden = () => {
+    return (
+        <>
+            <h1>403</h1>
+            <p>Forbidden</p>
+            <button onClick={() => window.history.back()}>Go back</button>
+        </>
+    );
+};
+const PrivateRoute = ({ element: Element, ...rest }) => {
+    const token = localStorage.getItem('token');
+    const [isLogged, setIsLogged] = useState(token !== "null");
+
+    useEffect(() => {
+        setIsLogged(localStorage.getItem('token') !== "null");
+    }, [token]);
+
+    return (
+        isLogged ? <Element {...rest} /> : <PageForbidden />
+    );
+};
+
+PrivateRoute.propTypes = {
+    element: PropTypes.node,
+};
+
+export default PrivateRoute;
