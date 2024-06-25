9465bc71604b63d8f7e42630376e4d6a60b6ffa6
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-06-25 10:58:49 -0400

    bug accessing flight info

--- arqui_frontend/src/Tickets/MisVuelos.jsx
+++ arqui_frontend/src/Tickets/MisVuelos.jsx
@@ -30,14 +30,14 @@ function EstadoCompras() {
             {aprovedRequests.map((request, index) => (
                 <div key={index} className='ticket-container' style={{ top: `${330 + index * 200}px` }}>
                     <div className='ticket-distribución'>
-                        <h2>{request.departure_time.slice(11,16)} {request.flight_info[0].departure_airport_id}</h2>
+                        <h2>{request.departure_time.slice(11,16)} {request.flight_info.departure_airport_id}</h2>
                         <h3>Duración</h3>
-                        <h2>{request.flight_info[0].arrival_time.split(" ")[1]} {request.flight_info[0].arrival_airport_id}</h2>
+                        <h2>{request.flight_info.arrival_time.split(" ")[1]} {request.flight_info.arrival_airport_id}</h2>
                         <h3>Tarifa desde</h3>
                         <p></p>
-                        <p>{Math.floor(request.flight_info[0].duration / 60)}h {request.flight_info[0].duration % 60} min</p>
+                        <p>{Math.floor(request.flight_info.duration / 60)}h {request.flight_info.duration % 60} min</p>
                         <p></p>
-                        <p>CLP {request.flight_info[0].price}</p>
+                        <p>CLP {request.flight_info.price}</p>
                     </div>
                     <hr className='line'/>
                     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
