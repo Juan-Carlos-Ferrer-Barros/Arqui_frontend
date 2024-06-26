5a86d84993cba9c2f7f3a1e0f0280057add3db04
javieranecochea <jnecochea1@uc.cl>
2024-06-25 21:36:41 -0400

    Entrega 2

--- arqui_frontend/src/Tickets/Recommendations.jsx
+++ arqui_frontend/src/Tickets/Recommendations.jsx
@@ -10,11 +10,11 @@ export default function Recommendations() {
 
   const token = localStorage.getItem('token');
   const isLogged = token !== "null";
-  // const [flightIds, setFlightIds] = useState([]);
-  // const [flights, setFlights] = useState([]);
+
   const [recommendations, setRecommendations] = useState([]);
   const [selectedFlight, setSelectedFlight] = useState(null);
   const navigate = useNavigate();
+  
 
   const realizarPosibleCompra = (flight) => {
     if (!isLogged) {
@@ -36,25 +36,18 @@ export default function Recommendations() {
   }, []);
 
   console.log('Recomendaciones:', recommendations);
-  // useEffect(() => {
-  //   flightIds.forEach(flightId => {
-  //     fetch(`${import.meta.env.VITE_API_URL}/flights/${flightId}`,)
-  //       .then(response => setFlights([...flights, response.json()]))
-  //       .catch(error => console.error('Error fetching flight information:', error));
-  //   });
-  // }, [flightIds]);
+
   const filteredRecommendations = recommendations.filter(recommendation => recommendation.flights.length > 0);
   return (
     <div className='scroll'>
       {filteredRecommendations.length === 0 ? <h1 className='titleNoNavbar'>No hay recomendaciones</h1>: <h1 className='titleNoNavbar'>Recomendaciones</h1>}
       {filteredRecommendations.length > 0 &&
-        filteredRecommendations.map((recommendation, index) => (
-            <div key={index}>
-              <h1 className='titleNoNavbar'>Recomendaciones de vuelos que salen de {recommendation.flights[0].departure_airport_id}</h1>
-              <h1 className='titleNoNavbar'>Creada el {recommendation.createdAt.split('T')[0]}</h1>
+        filteredRecommendations.map((recommendation, i) => (
+            <div key={i}>
+               <div className='recomendacion' style={{ top: `${250 + i * 650}px` }}>Recomendaciones de vuelos que salen de {recommendation.flights[0].departure_airport_id} - Creada el {recommendation.createdAt.split('T')[0]}</div> 
               {recommendation.flights.map((flight, index) => (
-                <button key={index}>
-                  <div key={index} className={`ticket-container ${selectedFlight === index ? 'selected' : ''}`} onClick={() => setSelectedFlight(index)} style={{ top: `${490 + index * 200}px` }}>
+                <div key={index}>
+                  <div key={index} className={`ticket-container ${selectedFlight === index ? 'selected' : ''}`} onClick={() => setSelectedFlight(index)} style={{ top: `${390 + i * 650 + index * 200}px` }}>
                     <div className='ticket-distribución'>
                       <h2>{flight.departure_time.slice(11, 16)} {flight.departure_airport_id}</h2>
                       <h3>Duración</h3>
@@ -73,43 +66,17 @@ export default function Recommendations() {
                       )}
                     </div>
                   </div>
-                </button>
+                </div>
               ))}
             </div>
+
         ))
       }
+      <div className='recomendacion' style={{ top: `${250 + filteredRecommendations.length * 650}px` }}> </div> 
+      
+
     </div>
+
     
-    // <div className='scroll'>
-    //   <h1 className='titleNoNavbar'>Recomendaciones</h1>
-    //   {recommendations.length === 0 && <h1 className='titleNoNavbar'>No hay recomendaciones</h1>}
-    //   {recommendations.length > 0 &&
-    //     recommendations.map((flights, index) => (
-    //       flights.length === 0 && <h1 className='titleNoNavbar'>------------------------</h1>
-    //       flights.length > 0 &&
-    //       flights.map((flight, index) => (
-    //         <button key={index}>
-    //           <div key={index} className={`ticket-container ${selectedFlight === index ? 'selected' : ''}`} onClick={() => setSelectedFlight(index)} style={{ top: `${490 + index * 200}px` }}>
-    //             <div className='ticket-distribución'>
-    //               <h2>{flight.departure_time.slice(11, 16)} {flight.departure_airport_id}</h2>
-    //               <h3>Duración</h3>
-    //               <h2>{flight.arrival_time.split(" ")[1]} {flight.arrival_airport_id}</h2>
-    //               <h3>Tarifa desde</h3>
-    //               <p></p>
-    //               <p>{Math.floor(flight.duration / 60)}h {flight.duration % 60} min</p>
-    //               <p></p>
-    //               <p>CLP {flight.price}</p>
-    //             </div>
-    //             <hr className='line' />
-    //             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-    //               <p className='tipo'>Directo</p>
-    //               {selectedFlight === index && (
-    //                 <button className='buy-ticket' onClick={() => realizarPosibleCompra(flight)}>Comprar pasaje</button>
-    //               )}
-    //             </div>
-    //           </div>
-    //         </button>
-    //     ))
-    // </div>
   )
 }
